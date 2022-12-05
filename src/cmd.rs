827035218c3bba7ab4be4bcbb6cb8c22bf038554
zhangtao <zhangtao@clamc.com>
2022-12-05 12:10:18 +0800

    尝试使用anyhow和thiserror

--- src/cmd.rs
+++ src/cmd.rs
@@ -1,19 +1,19 @@
 use std::process::{Output, Command};
-
+use anyhow::Result;
 use encoding::{all::GBK, Encoding};
 
 
-fn command_wrapper(cmd: &String) -> Output {
+fn command_wrapper(cmd: &String) -> Result<Output> {
     if cfg!(target_os = "windows") {
-        Command::new("cmd").arg("/C").arg(cmd).output().unwrap()
+        Ok(Command::new("cmd").arg("/C").arg(cmd).output()?)
     } else {
-        Command::new("sh").arg("-c").arg(cmd).output().unwrap()
+        Ok(Command::new("sh").arg("-c").arg(cmd).output()?)
     }
 }
-fn encoding_wrapper(err_vec: &Vec<u8>) -> String {
+fn encoding_wrapper(err_vec: &Vec<u8>) -> Result<String> {
     if cfg!(target_os = "windows") {
-        GBK.decode(err_vec, encoding::DecoderTrap::Ignore).unwrap()
+        Ok(GBK.decode(err_vec, encoding::DecoderTrap::Ignore).unwrap_or_default())
     } else {
-        String::from_utf8_lossy(err_vec).to_string()
+        Ok(String::from_utf8_lossy(err_vec).to_string())
     }
 }
